09204de05f6bf7d6d17aed2a956d5132bddb4c58
Mohamed Shalaby <mohmahsh2005@gmail.com>
2026-04-19 15:55:26 +0200

    Add files via upload

---
+++ pages/Layout/Navbar.php
@@ -0,0 +1,17 @@
+<section id="header">
+    <a href="#"><img src="../assets/images/ma_logo.png" class="logo" alt="logo"></a>
+
+    <div>
+        <ul id="navbar">
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="shop.php">SHOP</a></li>
+            <li><a href="about.php">ABOUT</a></li>
+            <li><a href="contact.php">CONTACT US</a></li>
+            <li><a href="SignIn.php">LOGIN</a></li>
+            <a href="#" id="close"><i class="fa-solid fa-x"></i></a>
+        </ul>
+    </div>
+    <div id="mobile">
+        <i id="bar" class="fa-solid fa-bars"></i>
+    </div>
+</section>
