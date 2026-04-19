57bc2f90f9493e05118e44c563f30a318679b3f3
Mohamed Shalaby <mohmahsh2005@gmail.com>
2026-04-19 15:51:21 +0200

    Add files via upload

---
+++ DB/ma_store.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS ma_store_db;
+USE ma_store_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
